e047b4ded6059f50093e591fc0913464b91c8b8c
William Yang Yang <yywill@gmail.com>
2012-06-24 17:45:50 +0800

    init

---
+++ src/main/java/org/williamYang/App.java
@@ -0,0 +1,13 @@
+package org.williamYang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
